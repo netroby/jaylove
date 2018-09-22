173f10360a11f3e9157ee3643e27b3fff6b888bd
ZhiFeng Hu <hufeng1987@gmail.com>
2018-09-22 20:49:51 +0800

    Create database

---
+++ src/main/kotlin/com/netroby/jaylove/JayloveApplication.kt
@@ -0,0 +1,11 @@
+package com.netroby.jaylove
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JayloveApplication
+
+fun main(args: Array<String>) {
+    runApplication<JayloveApplication>(*args)
+}
